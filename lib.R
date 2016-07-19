a3815d54898d1a5b3bf72da6ebee0e9ca4da46c0
kdaily <kmdaily@gmail.com>
2016-07-19 15:29:31 -0700

    move doQuery to lib

--- lib.R
+++ lib.R
@@ -1,3 +1,15 @@
+doQuery <- function(con, template, projectId, beginTimestamp, endTimestamp) {
+  q.browse <- sprintf(template, projectId, beginTimestamp, endTimestamp)
+  
+  data <- dbGetQuery(conn = con, statement=q.browse) %>% 
+    dplyr::rename(userid=USER_ID, id=ENTITY_ID)
+  
+  data %>% filter(RESPONSE_STATUS == 200)  %>% 
+    count(userid, id, DATE, TIMESTAMP, NODE_TYPE, NAME) %>% 
+    ungroup()
+  
+}
+
 getTeamMemberDF <- function(teamId) {
   userListREST <- synRestGET(sprintf("/teamMembers/%s?limit=500", teamId))
   userList <- ldply(userListREST$results,
