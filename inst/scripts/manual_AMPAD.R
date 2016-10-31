48ebd31b18fc35a68f222f26521e8a46a427a82b
kdaily <kmdaily@gmail.com>
2016-10-31 16:43:12 -0700

    add ampad script

---
+++ inst/scripts/manual_AMPAD.R
@@ -0,0 +1,23 @@
+templates <- c("webAccess"="../webAccess.Rmd", 
+               "downloads"="../downloads.Rmd")
+
+### MEP-LINCS
+projectId <- 'syn2580853'
+reportType <- "webAccess"
+
+myParams <- list(projectId=projectId, 
+                 nMonths=12, 
+                 aclTeamOrder=c(3346847, projectId), 
+                 useTeamGrouping=TRUE)
+
+reportType <- "webAccess"
+rmarkdown::render(input=templates[[reportType]],
+                  output_file=paste0(myParams[['projectId']], "_", reportType, "_",
+                                     lubridate::today(), ".html"),
+                  params = myParams)
+
+reportType <- 'downloads'
+rmarkdown::render(input=templates[[reportType]],
+                  output_file=paste0(myParams[['projectId']], "_", reportType, "_",
+                                     lubridate::today(), ".html"),
+                  params = myParams)
