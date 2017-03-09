5b64b28c2cb133ca23b1b404670200963dfaca81
Kenneth Daily <kenneth.daily@sagebase.org>
2017-03-09 13:36:02 -0500

    simplify.

--- inst/scripts/manual_AMPAD.R
+++ inst/scripts/manual_AMPAD.R
@@ -7,23 +7,11 @@ projectId <- 'syn2580853'
 reportType <- "webAccess"
 
 myParams <- list(projectId=projectId, 
-                 nMonths=23, 
+                 nMonths=26,
                  aclTeamOrder=c(3346847, 3320424, projectId), 
                  useTeamGrouping=TRUE)
 
 reportType <- "report"
-rmarkdown::render(input=templates[[reportType]],
-                  output_file=paste0(myParams[['projectId']], "_", reportType, "_",
-                                     lubridate::today(), ".html"),
-                  params = myParams)
-
-reportType <- 'downloads'
-rmarkdown::render(input=templates[[reportType]],
-                  output_file=paste0(myParams[['projectId']], "_", reportType, "_",
-                                     lubridate::today(), ".html"),
-                  params = myParams)
-
-reportType <- "webAccess"
 rmarkdown::render(input=templates[[reportType]],
                   output_file=paste0(myParams[['projectId']], "_", reportType, "_",
                                      lubridate::today(), ".html"),
