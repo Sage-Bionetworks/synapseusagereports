2f0485c45ae8cd22f7d2d998b111bf1c4ea4208d
kdaily <kenneth.daily@sagebase.org>
2018-12-07 14:28:35 -0800

    remove reference to acl team order

--- inst/scripts/manual_AMPAD.sh
+++ inst/scripts/manual_AMPAD.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 project_id="syn2580853"
-acl_team_order='3377637,3346847'
+team_order='273957,3377637,3346847,3320424'
 
 # 6 months ago
 start_date=$(date --date='-6 months' -I -u)
@@ -10,4 +10,4 @@ start_date=$(date --date='-6 months' -I -u)
 end_date=`date -u -I`
 
 ./report_data_query.R --project_id=${project_id} --start_date=${start_date} --end_date=${end_date} --config_file=~/datawarehouse-before-aug.yml > /tmp/${project_id}_${end_date}.csv
-./render_report.R  --project_id=${project_id} --acl_team_order=${acl_team_order} /tmp/${project_id}_${end_date}.csv
+./render_report.R  --project_id=${project_id} --team_order=${team_order} /tmp/${project_id}_${end_date}.csv
--- tests/testthat/test-lib.R
+++ tests/testthat/test-lib.R
@@ -1,8 +1,2 @@
 context("test-lib")
 
-test_acl_json <- '{"id":"syn12509922","resourceAccess":[{"principalId":3323072,"accessType":["DELETE","DOWNLOAD","READ","CHANGE_SETTINGS","UPDATE","MODERATE","CREATE","CHANGE_PERMISSIONS"]}],"etag":"19a13eb7-8d24-4d26-b9d7-b8fa97b8ce92","creationDate":"2018-06-11T18:41:45.244Z","uri":"/entity/syn12509922/acl"}'
-test_acl <- jsonlite::fromJSON(test_acl_json, simplifyVector = FALSE)
-
-test_that("acl to user list", {
-  expect_equal(aclToMemberList(test_acl), )
-})
