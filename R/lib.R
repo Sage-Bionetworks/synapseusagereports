1be00e94f7e4e62f4cce8f513413ad7c0a06d31a
kdaily <kenneth.daily@sagebase.org>
2020-04-27 09:48:54 -0700

    move plotting functions to separate module

--- R/lib.R
+++ R/lib.R
@@ -1,11 +1,6 @@
 #' @importFrom dplyr %>%
 #' @importFrom lubridate %m+%
 
-# Theme for plots
-mytheme <- ggplot2::theme_bw() + ggplot2::theme(axis.text = ggplot2::element_text(size = 16),
-                                                axis.title.x = ggplot2::element_text(size = 18),
-                                                axis.title.y = ggplot2::element_text(size = 18, angle = 90))
-
 #' @export
 render_report <- function(project_id, team_order, data_file, reportType = "report") {
 
@@ -296,29 +291,6 @@ countByDay <- function(queryData, useTeamGrouping) {
 
 }
 
-#' @export
-plotByDay <- function(perdayCount, useTeamGrouping) {
-  plotdata <- perdayCount %>%
-    reshape2::dcast(date ~ teamName, value.var = 'n', fill = 0) %>%
-    reshape2::melt(., id.vars = c("date"),
-                   variable.name = "teamName", value.name = "n") %>%
-    dplyr::rename(group = teamName)
-
-  p <- ggplot2::ggplot(plotdata, ggplot2::aes(x = date, y = n))
-  p <- p + ggplot2::geom_line(ggplot2::aes(group = group, color = group), size = 1)
-
-  if (useTeamGrouping) {
-    p <- p + ggplot2::scale_color_brewer(palette = "Set1")
-  } else {
-    p <- p + ggplot2::scale_color_manual(values = "black")
-  }
-
-  p  <- p + mytheme + ggplot2::theme(axis.title.x = ggplot2::element_blank(),
-                                     axis.text.x = ggplot2::element_text(size = 16, angle = 270),
-                                     legend.position = "top")
-  p
-
-}
 
 #' @export
 uniqueUsersPerMonth <- function(queryData) {
